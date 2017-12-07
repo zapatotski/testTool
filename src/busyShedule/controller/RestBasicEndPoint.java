a8af0842d25f99648fead53af1c52554cfe895df
Igor <zapototski@gmail.com>
2017-12-07 20:28:37 +0200

    cvbc

--- src/busyShedule/controller/RestBasicEndPoint.java
+++
@@ -1,8 +0,0 @@
-package busyShedule.controller;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("shedule")
-public class RestBasicEndPoint extends Application {
-}
